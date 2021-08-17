2b0bb603c2f1ce7ed363abf14992159ab2b24b30
Shinee <shineenmkh@gmail.com>
2021-08-18 06:18:37 +1000

    webpack bolon webpack-cli suulgasan package.json bolon webpack.config.js file-uudiin tohirgoo bolon watch tohirgoog hiisen

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+{
+    const path = require("path");
+
+    module.exports = {
+
+        entry:"./app/assets/scripts/app.js",
+        output:
+        {
+            filename:"bundled.js",
+            path:path.resolve(__dirname, "./docs")
+        },
+        mode:"development",
+        watch:true
+    }
+}
